386b13576b4596ab2afb8f3b3149238b1ea73ebd
sumitdorle <sumitdorle@gmail.com>
2015-04-22 16:04:31 +0000

    added random movement to the fire sprite

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var shark,
     cursors,
-    Bug;
+    Bug,
+    bullet;
 var bugs = [];
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
@@ -8,25 +9,26 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 function preload(){
     game.load.image('shark', 'shark.png');
     game.load.image('bug', 'bug.png');
+    game.load.image('bullet', 'bullet.png');
 }
 
-Bug = function(){
-        this.x = game.world.randomX;
-        this.y = game.world.randomY;
-        //this.minSpeed = -75;
-        //this.maxSpeed = 75;
-        //this.vx = Math.random()*(this.maxSpeed - this.minSpeed+1)-this.minSpeed;
-        //this.vy = Math.random()*(this.maxSpeed - this.minSpeed+1)-this.minSpeed;
+//Bug = function(){
+        //this.x = game.world.randomX;
+        //this.y = game.world.randomY;
+        // this.minSpeed = -75;
+        // this.maxSpeed = 75;
+        // this.vx = Math.random()*(this.maxSpeed - this.minSpeed+1)-this.minSpeed;
+        // this.vy = Math.random()*(this.maxSpeed - this.minSpeed+1)-this.minSpeed;
+        // alert(vx);
 
-
-        this.bugSprite = game.add.sprite(this.x,this.y,"bug");
-        this.bugSprite.anchor.setTo(0.5, 0.5);
+        //this.bugSprite = game.add.sprite(this.x,this.y,"bug");
+        //this.bugSprite.anchor.setTo(0.5, 0.5);
         // this.bugSprite.body.collideWorldBounds = true;
         // this.bugSprite.body.bounce.setTo(1, 1);
         // this.bugSprite.body.velocity.x = this.vx;
         // this.bugSprite.body.velocity.y = this.vy;
         // this.bugSprite.body.immovable = true;
-    }
+//    }
 
 
 
@@ -42,18 +44,30 @@ function create(){
     shark.body.drag.x = 100;
     shark.anchor.setTo(0.5, 1);
     
-    // //bug   
-    // bugs = game.add.group()
-    // createbugs(bugs);
-    // game.physics.arcade.enable(bugs);
-    // cursors = game.input.keyboard.createCursorKeys();
+    //bug   
+    bugs = game.add.group();
+    createbugs(bugs);
+    game.physics.arcade.enable(bugs);
+    
+    
+    //bullet
+    bullet = this.add.sprite(this.world.randomX, this.world.randomY, 'bullet');
+    game.physics.arcade.enable(bullet);
+    bullet.enableBody = true;
+    bullet.body.bounce.y = 0.2;
+    //bullet.body.gravity.y = 500;
+    bullet.body.collideWorldBounds = true;
+    game.time.events.repeat(Phaser.Timer.SECOND, 10, randomMovement, game);
+    
+    cursors = game.input.keyboard.createCursorKeys();
 
-    for (var i=0; i<7; i++) {
-        bugs.push( new Bug() );
-    }
+    // for (var i=0; i<7; i++) {
+    //     bugs.push( new Bug() );
+    // }
 }
 
 function update(){
+    
     moveshark(shark);
     game.physics.arcade.collide(shark, bugs, killbug, null, this);
 }
@@ -66,12 +80,27 @@ function killbug(){
 }
 
 
-// function createbugs(obj){
-//     for (var i = 0; i < 5; i++)
-//     {
-//         obj.create(500 + Math.random() * 200, 200 + Math.random() * 200, 'bug');
-//     }
-// }
+function createbugs(obj){
+    for (var i = 0; i < 5; i++)
+    {
+        obj.create(500 + Math.random() * 200, 200 + Math.random() * 200, 'bug');
+    }
+}
+
+
+function randomMovement() {
+    //randomise the movement        
+    var bulletmover = game.rnd.integerInRange(1, 4);
+    if (bulletmover == 1) {
+        bullet.body.velocity.x = 50;       
+    } else if(bulletmover == 2) {   
+        bullet.body.velocity.x = -50;    
+    } else if(bulletmover == 3) {   
+        bullet.body.velocity.y = -50;    
+    } else if(bulletmover == 4) {   
+        bullet.body.velocity.y = 50;    
+    } 
+}
 
 
 function moveshark(shark)
